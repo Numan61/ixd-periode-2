99f7f017dfcfa22ac8ae01e3b8e4ce56e822fd59
Numan61 <37619174+Numan61@users.noreply.github.com>
2019-02-11 16:55:43 +0100

    Updated version

--- Challenge 1/js/code.js
+++ Challenge 1/js/code.js
@@ -2,6 +2,7 @@
 google.charts.setOnLoadCallback(drawChartDonut);
 google.charts.setOnLoadCallback(drawChartBar);
 google.charts.setOnLoadCallback(drawChartGauge);
+google.charts.setOnLoadCallback(drawChartColum);
       function drawChartDonut() {
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
@@ -42,21 +43,21 @@ google.charts.setOnLoadCallback(drawChartGauge);
         bar: {groupWidth: "95%"},
         legend: { position: "none" },
       };
-      var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+      var chart = new google.visualization.BarChart(document.getElementById("gaugechart"));
       chart.draw(view, options);
   }
 
+google.charts.load('current', {'packages':['gauge']});
 function drawChartGauge() {
 
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
-          ['Memory', 80],
-          ['CPU', 55],
-          ['Network', 68]
+          ['Zwaartekracht', 80],
+          ['Brandstof', 55],
         ]);
 
         var options = {
-          width: 400, height: 120,
+          width: 400, height: 400,
           redFrom: 90, redTo: 100,
           yellowFrom:75, yellowTo: 90,
           minorTicks: 5
@@ -78,4 +79,23 @@ function drawChartGauge() {
           data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
           chart.draw(data, options);
         }, 26000);
+      }
+
+ google.charts.load('current', {'packages':['bar']});
+   function drawChartColum() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Mannen', 'Vrouwen'],
+          ['2030', 52, 51]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Hoeveel personen nemen zich deel aan '
+           
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
       }
